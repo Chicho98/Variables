c4857f3588cde0a7a5e9cc4704fbc38b12b3d79a
Chicho98 <leecordeiro99@gmail.com>
2023-09-11 19:53:56 -0300

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿const int x = 2;
+const int y = 3;
+char symbol = '@';
+double z = 1.22;
+string a = "omagod";
+
+Console.Write(x+y);
+
+
+
+bool jimmy  = true;
+
+Console.Write(symbol);
+Console.WriteLine(z);
+Console.Write(jimmy);
+Console.WriteLine(a);
+Console.ReadKey();
+/*las variables son valores numericos, simbolos, numeros decimales, verdadero o falso y lineas de caracteres.
+ * las constantes son valores QE NO SE PUEDEN CAMBIAR durante la vida util del programa y su compilacion se usa cuando
+ * se declara una variable para mantenerla fija qe no se pueda cambiar , agrega seguridad al programa
+*/
